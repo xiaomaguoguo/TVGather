e4a569e3f2cbdd63295607bd28307963b4e3f578
mazhihua <mazhihua@bftv.com>
2017-09-14 14:34:09 +0800

    性能优化 ，调整多线程对数据库的读写策略

--- TVGather/app/src/main/java/activity/MyApplication.java
+++ TVGather/app/src/main/java/activity/MyApplication.java
@@ -1,6 +1,7 @@
 package activity;
 
 import android.app.ActivityManager;
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -23,31 +24,31 @@ import utils.DexoptUtils;
  * Created by MaZhihua on 2017/9/4.
  */
 
-public class MyApplication extends MultiDexApplication {
+public class MyApplication extends /*MultiDexApplication*/ Application{
 
     public static final String KEY_DEX2_SHA1 = "dex2-SHA1-Digest";
 
     public static final String TAG = MyApplication.class.getSimpleName();
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        if (!DexoptUtils.quickStart(base) && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {//>=5.0的系统默认对dex进行oat优化
-            if (DexoptUtils.needWait(base)){
-                DexoptUtils.waitForDexopt(base);
-            }
-            MultiDex.install (base );
-        } else {
-            return;
-        }
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (DexoptUtils.quickStart(this)) {
-            return;
-        }
-    }
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        if (!DexoptUtils.quickStart(base) && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {//>=5.0的系统默认对dex进行oat优化
+//            if (DexoptUtils.needWait(base)){
+//                DexoptUtils.waitForDexopt(base);
+//            }
+//            MultiDex.install (base );
+//        } else {
+//            return;
+//        }
+//    }
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        if (DexoptUtils.quickStart(this)) {
+//            return;
+//        }
+//    }
 
 }
